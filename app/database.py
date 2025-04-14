48498e36f9bea1df79c27d565a1a265d145a9c42
ashwin001-gif <ashwinrajput503@gmail.com>
2025-04-14 17:27:06 +0530

    Implemented features for articles, comments, likes, search, and more

--- app/database.py
+++ app/database.py
@@ -1,19 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
-import os
+from . import models
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./news.db")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Change this to your preferred database URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
