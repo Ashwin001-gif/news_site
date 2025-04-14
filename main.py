bba1e57ca1a1ac652dc0fa6320c941379ce41b09
ashwin001-gif <ashwinrajput503@gmail.com>
2025-04-14 15:28:54 +0530

    Fix startCommand in render.yaml

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
 
+from app.routers import articles
+from app.database import engine
+from app.models import Base
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the News Site!"}
-
-from app.routers import articles
-from app.database import engine
-from app.models import Base
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="News Website API")
-
-app.include_router(articles.router)
