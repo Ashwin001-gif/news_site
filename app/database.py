e0fd851e7f5ef33cb0fc3c269703706336864257
ashwin001-gif <ashwinrajput503@gmail.com>
2025-04-14 21:38:01 +0530

    fix: correct import from database instead of db_connection

--- app/database.py
+++ app/database.py
@@ -2,16 +2,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# Create the base object for model classes
-Base = declarative_base()
+
 # Database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Change this as per your configuration
+SQLALCHEMY_DATABASE_URL = "sqlite:///./news.db"  # Change this as per your configuration
 # Create engine and session maker
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# This function will be called later to import models and initialize the database
-def get_db():
-    from . import models  # Import models here, after the Base object is initialized
-    return SessionLocal()
 
 Base = declarative_base()
+
+from .db_connection import SessionLocal
+
+# Function to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
